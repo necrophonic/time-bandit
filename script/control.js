52de389900487943d4659de0c2e775a4bcd6f012
John Gregory <jgregory@companieshouse.gov.uk>
2015-08-11 16:47:54 +0100

    Move to anonymous function

--- script/control.js
+++ script/control.js
@@ -1,24 +1,26 @@
+// Start or stop the timing and write a start/stop point to the time record.
+start_stop = function() {
+  getRecord('',function(record) {
+    var ms = msSinceMidnight()
+    if (running) {
+      console.log("Stop timing @ "+ms)
+      running = false
+    } else {
+      console.log("Start timing @ "+ms)
+      running = true
+    }
+    $("#run-time").toggleClass('faded');
+    $("#pause").toggleClass('faded');
+    var times = record['times']
+    times.push(ms)
+    updateRecord(record['id'],{"times":times},function() {
+      return;
+    });
+  })
+}
+
 $(function() {
 
-  $("#run-control").click(function() {
-    // Start or stop the timing and write a start/stop point to the time record.
-    getRecord('',function(record) {
-      var ms = msSinceMidnight()
-      if (running) {
-        console.log("Stop timing @ "+ms)
-        running = false
-      } else {
-        console.log("Start timing @ "+ms)
-        running = true
-      }
-      $("#run-time").toggleClass('faded');
-      $("#pause").toggleClass('faded');
-      var times = record['times']
-      times.push(ms)
-      updateRecord(record['id'],{"times":times},function() {
-        return;
-      });
-    });
-  });
+  $("#run-control").click(start_stop);
 
 });
